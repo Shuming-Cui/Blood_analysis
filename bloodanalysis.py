4c53fe69cf8ffddf6caa85404bdcde95112a9e6b
崔书铭 <cuishuming@CuigedeMacBook.local>
2020-01-24 12:42:31 -0500

    Add cholesterol test

--- bloodanalysis.py
+++ bloodanalysis.py
@@ -1,4 +1,20 @@
 # bloodanalysis
+def HDL_analysis(HDL_level):
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else :
+        return "Low"
+    
+
+def cholesterol_analysis():
+    print ("Cholesterol Analysis")
+    HDLinput = input("Enter test result: ")
+    test_info = HDLinput.split("=")
+    if test_info[0] == "HDL":
+        answer = HDL_analysis(int(test_info[1]))
+        print("The level is {}".format(answer))
 
 
 def interface():
@@ -6,10 +22,13 @@ def interface():
     while choice !=9:
         print("Cholesterol calculator")
         print("Options:")
+        print(" 1 - Cholesterol Analysis")
         print(" 9 - Quit")
-        choice = input("Enter")
+        choice = input("Enter: ")
         if choice == '9':
             return
+        elif choice == '1':
+            cholesterol_analysis()
 
 if __name__ == '__main__':
     interface()
